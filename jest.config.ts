555fb2595b936c3b8ffb16528a7983acdbec8ead
yasuhikasa <yasuhikasa@yahoo.co.jp>
2024-08-25 22:44:11 +0900

    jestの初期設定

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+};
+
+export default createJestConfig(customJestConfig);
