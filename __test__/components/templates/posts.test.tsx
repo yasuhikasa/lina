c42f017f6e3d335906aca0cf4dfacf426a11bdd1
yasuhikasa <yasuhikasa@yahoo.co.jp>
2024-08-26 02:18:48 +0900

    postsのテスト作成

---
+++ __test__/components/templates/posts.test.tsx
@@ -0,0 +1,45 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import Posts from '@/components/templates/posts';
+import mockRouter from 'next-router-mock';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '@/libs/firebaseConfig';
+
+// Firebase hooks と next/router をモック
+jest.mock('react-firebase-hooks/auth', () => ({
+  useAuthState: jest.fn(),
+}));
+jest.mock('next/router', () => require('next-router-mock'));
+
+describe('Posts Component', () => {
+  const mockPush = jest.fn();
+
+  beforeEach(() => {
+    mockRouter.setCurrentUrl('/posts');
+    mockRouter.push = mockPush;
+    jest.clearAllMocks();
+  });
+
+  it('ログインしていない場合、トップページにリダイレクトされることを確認する', async () => {
+    (useAuthState as jest.Mock).mockReturnValue([null, false]);
+
+    render(<Posts />);
+
+    await waitFor(() => {
+      expect(mockPush).toHaveBeenCalledWith('/');
+    });
+  });
+
+  it('ログインしている場合、投稿ページが正しくレンダリングされることを確認する', async () => {
+    const mockUser = { uid: 'test-uid' };
+    (useAuthState as jest.Mock).mockReturnValue([mockUser, false]);
+
+    render(<Posts />);
+
+
+    // 新規投稿ボタンが表示されるのを確認する
+    await waitFor(() => {
+      expect(screen.getByRole('button', { name: '新規投稿' })).toBeInTheDocument();
+    });
+  });
+
+});
